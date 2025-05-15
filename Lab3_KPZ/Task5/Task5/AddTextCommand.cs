d7f7e4c1409e27cdceda24371352a3fe10a45ed0
Юрчук Артем Вячеславович <vt231_yuav@student.ztu.edu.ua>
2025-05-15 12:46:58 +0300

    Add ICommand

---
+++ Lab3_KPZ/Task5/Task5/AddTextCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    public class AddTextCommand : ICommand
+    {
+        private readonly LightElementNode _element;
+        private readonly string _text;
+
+        public AddTextCommand(LightElementNode element, string text)
+        {
+            _element = element;
+            _text = text;
+        }
+
+        public void Execute()
+        {
+            _element.AddChild(new LightTextNode(_text));
+        }
+    }
+
+}
---
+++ Lab3_KPZ/Task5/Task5/CommandInvoker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    public class CommandInvoker
+    {
+        private readonly List<ICommand> _commands = new List<ICommand>();
+
+        public void Invoke(ICommand command)
+        {
+            _commands.Add(command);
+            command.Execute();
+        }
+
+        public void Undo()
+        {
+            
+        }
+    }
+
+}
